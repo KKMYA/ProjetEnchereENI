0c3da3fb27e3882d5eb47d9fb4530c92f1795689
KKMYA <156657977+KKMYA@users.noreply.github.com>
2024-01-15 18:04:41 +0100

    init
    init

---
+++ src/main/java/fr/eni/enchere/projet/gestion/ServletAcceuil.java
@@ -0,0 +1,32 @@
+package fr.eni.enchere.projet.gestion;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletAcceuil
+ */
+
+@WebServlet("")
+public class ServletAcceuil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletAcceuil() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("TEST TEST TEST");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
