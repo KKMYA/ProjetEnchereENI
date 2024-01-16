ec56e86855e8d6e0bf5640376ca97deba7ff0071
K000kai <156654837+K000kai@users.noreply.github.com>
2024-01-16 10:25:21 +0100

    test test

--- src/main/java/fr/eni/enchere/projet/gestion/ServletAcceuil.java
+++ src/main/java/fr/eni/enchere/projet/gestion/ServletAcceuil.java
@@ -28,6 +28,8 @@ public class ServletAcceuil extends HttpServlet {
 		response.getWriter().append("TEST TEST TEST");
 		response.getWriter().append("TEST TEST TEST");
 		response.getWriter().append("TEST TEST TEST");
+		response.getWriter().append("TEST TEST TEST");
+		response.getWriter().append("TEST TEST TEST");
 
 	}
 
