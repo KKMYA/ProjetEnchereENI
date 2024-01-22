e3872a01262ea17397d11d9267870261787289c8
KKMYA <156657977+KKMYA@users.noreply.github.com>
2024-01-22 10:36:57 +0100

    Creation session
    - Check de session sur chacune des servlets afin d'identifier l'utilisateur présent sur la page

--- src/main/java/fr/eni/enchere/projet/gestion/ServletFIcheEnchere.java
+++ src/main/java/fr/eni/enchere/projet/gestion/ServletFIcheEnchere.java
@@ -9,6 +9,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = "/FicheEnchere")
 public class ServletFIcheEnchere extends HttpServlet {
@@ -18,6 +19,7 @@ public class ServletFIcheEnchere extends HttpServlet {
        
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(false);
 		request.setAttribute("Description", articleDAO);
 	}
 
