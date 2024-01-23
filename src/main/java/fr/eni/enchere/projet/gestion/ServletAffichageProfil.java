bb3b13edee2ae2c45c7996459af532b2ff2dd49b
KKMYA <156657977+KKMYA@users.noreply.github.com>
2024-01-23 16:37:38 +0100

    k;jhvcjygh

--- src/main/java/fr/eni/enchere/projet/gestion/ServletAffichageProfil.java
+++ src/main/java/fr/eni/enchere/projet/gestion/ServletAffichageProfil.java
@@ -1,9 +1,6 @@
 package fr.eni.enchere.projet.gestion;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import fr.eni.enchere.projet.bll.IdentificationPseudo;
 import fr.eni.enchere.projet.bo.Utilisateur;
@@ -16,45 +13,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-	/**
-	 * Servlet implementation class ServletAffichageProfil
-	 */
-	@WebServlet("/affichageProfil")
-	public class ServletAffichageProfil extends HttpServlet {
-		private static final long serialVersionUID = 1L;
-		UtilisateurDAO utilisateurdao = DAOFactory.getUtilisateurDAO();
+@WebServlet(urlPatterns = "/affichageProfil")
+public class ServletAffichageProfil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	UtilisateurDAO utilisateurdao = DAOFactory.getUtilisateurDAO();
 		
-		/**
-		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-		 */
-		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			HttpSession session = request.getSession(false); // Récupérer la session sans en créer une nouvelle
-			if (session != null && session.getAttribute("noUtilisateur") != null) {
-				int idSession = Integer.parseInt(session.getAttribute("noUtilisateur").toString());
-				//IdentificationPseudo.identification(idSession);
-				System.out.println(idSession);
-				
-				try {
-				
-				Utilisateur utilisateur =	utilisateurdao.select(idSession);
-				System.out.println(utilisateur);
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-			}
-			
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession(false); // Récupérer la session sans en créer une nouvelle
+		
+		if (session != null && session.getAttribute("noUtilisateur") != null) {
+			int idSession = Integer.parseInt(session.getAttribute("noUtilisateur").toString());								
+			Utilisateur utilisateur =	IdentificationPseudo.identification(idSession);
+			request.setAttribute("utilisateur", utilisateur);			
 			request.getRequestDispatcher("WEB-INF/affichageProfil.jsp").forward(request, response);;
-			
+			}
+			else {
+				System.out.println("Problème affichage");
+			}			
 			
 		}
 	
-		/**
-		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-		 */
+
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			// TODO Auto-generated method stub
 			doGet(request, response);
 		}
 	
